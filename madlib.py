74b592d0954ceeb390c58d65d939219b66f2ce10
abh2050 <44420081+abh2050@users.noreply.github.com>
2018-10-27 13:49:11 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,7 @@
+# madlib.py
+in_name = input('give me a name >')
+# give me a name > jason
+in_relative = input('give me a relative >')
+# give me a relative > brother
+in_verb = input('give me a verb >')
+print(f" My name is {in_name}, You killed my {in_relative}, prepare to die by {in_verb}")
