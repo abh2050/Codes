55417614afce982aefd203f83dcda3b64c505766
Abhishek Shah <44420081+abh2050@users.noreply.github.com>
2022-06-09 09:02:16 -0700

    Delete enough_sleep.py

--- enough_sleep.py
+++
@@ -1,9 +0,0 @@
-# enough_sleep.py
-user_sleep = input("How much did you sleep last night?\n>")
-user_sleep = int(user_sleep)
-if user_sleep <=6:
-    print("You failed! you have insomnia!")
-if user_sleep >=10:
-    print("You are sleeping too much")
-if user_sleep <10 and user_sleep > 6:
-    print("Takin care of yourself. Good job!")
